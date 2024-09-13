1805910abc4843d4ad39989773cb72ca691ab8d2
apustovoitova <anastasiapustovoitova@gmail.com>
2024-09-13 13:44:56 +0200

    deleted component

--- src/components/app-page/app-page.js
+++
@@ -1,7 +0,0 @@
-import "./app-page.css";
-
-const AppPage = () => {
-  return <div className="page"></div>;
-};
-
-export default AppPage;
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,6 @@
 import "./app.css";
 import AppHeader from "../app-header/app-header";
 import AppNavigation from "../app-navigation/app-navigation";
-import AppPage from "../app-page/app-page";
 import AppAbout from "../app-about/app-about";
 import AppBreadcrumbs from "../app-breadcrumbs/app-breadcrumbs";
 import AppButtonTop from "../app-button-on-top/app-button-on-top";
@@ -14,7 +13,6 @@ function App() {
       <AppHeader />
       <AppNavigation />
       <hr class=" divider-top" />
-      {/* <AppPage /> */}
       <AppBreadcrumbs />
       <AppAbout />
       <hr class="hr-red" styles="max-width: 50%" />
